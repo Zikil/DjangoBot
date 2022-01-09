5329511bb48b8e968cdc8715a3bfe539237754a6
Zik <il.zahvatkin@gmail.com>
2022-01-09 06:48:17 +0000

    pythonanywhere version

--- djangobot/djangobot/settings.py
+++ djangobot/djangobot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=%-slwt#e@qj29zw4tt8)4035(3%e3y7)m4dbd7g3sp!^b8x9=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zikil.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/home/Zikil/DjangoBot/djangobot/static"
+# or, eg,
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
